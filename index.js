74c872897a4f9a26600192e9055341278446b0aa
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2026-01-16 19:21:10 +0000

    UPDATE: Version change and name edit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
- *  orchish - 👹 Generate a random orc name
- *  @version: v1.2.4
+ *  orc-me - 👹 Generate a random orc name
+ *  @version: v1.2.5
  *  @link: https://github.com/tutyamxx/orc-me
  *  @license: MIT
  **/
